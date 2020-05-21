f9cadec1669f639a2b287d0ac476f513e0863c86
iqisq <58690713+iqisq@users.noreply.github.com>
2020-05-21 21:29:04 +0300

    Update swat.js

--- swat.js
+++ swat.js
@@ -1,59 +1,24 @@
 
-$( "button.btn.btn-primary" ).css( "border-radius", "24px 24px 24px 24px" )
-$(document).ready($("div.fr.borderg").css("display","none"));
-if(location.protocol!='https:'){location.protocol='https:';}
-$(".label-primary").css("border-radius","24px 24px 24px 24px")
+<!-- BEGINS: AUTO-GENERATED MUSES RADIO PLAYER CODE -->
+<script type="text/javascript" src="https://hosted.muses.org/mrp.js"></script>
+<script type="text/javascript">
+MRP.insert({
+'url':'http://stream.zeno.fm/ug2x6nx9nzzuv			',
+'codec':'mp3',
+'volume':100,
+'autoplay':false,
+'jsevents':true,
+'buffering':0,
+'title':'شات بغداديات للجوال',
+'welcome':'By : علي التميمي',
+'wmode':'transparent',
+'skin':'faredirfare',
+'width':269,
+'height':52
+});
+</script>
+<!-- ENDS: AUTO-GENERATED MUSES RADIO PLAYER CODE -->
+ <HTML>
 
-
-$(`
-
-`).insertBefore('.nav-tabs');
-$("div#tlogins button.btn.btn-primary").click(function(){
-    var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){ 
-    $(`
-        <div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
-          <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(pic/1dl1qds5g10.jpeg.jpg);" class="fl fitimg hand u-pic    ">
-          <div class="uzr fl" style="padding:0px;width:80%">
-            <div style="padding:0px;width:100%;" class="fl">
-              <img class="fl u-ico" alt="" src="sico/z1d545pgp610.jpg">
-              <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand">‎    طـــيبة ✪	  </span>
-            </div>
-            <br>
-            <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
-            <div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl"> مرحباً بك  <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ `</h1> نقدّر تواجدك معنا، ونتمنى لك يوماً سعيداً إن شاء الله </div>
-          </div>
-        </div></div>
-    `).appendTo('div#d2')
-    clearInterval(myVar);
-    
-    }else{console.log(usmsgw)} }, 2000);
-    })
-    
-
-$('.nav-tabs').addClass('fl').css('width','100%');$
-$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"><link rel="stylesheet" href="assl.css">
-</style>`).insertBefore('span.fl.fa.fa-send');
-
-$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
-<style>
-.fitimg.fl.u-pic  {box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(112, 112, 113);border-radius: 75px;}
-ul.nav.nav-tabs.fl li a {      color: #A1A1A1;  box-shadow: inset 0 2px 2px rgb(112, 112, 113), 0 0 8px rgb(112, 112, 113);font-size: 13px!important;padding: 6px!important;text-align: center!important;width: 100%;}
-ul.nav.nav-tabs.fl li {border-bottom: 3px;width: 33.3%;}
-.fr.co {
-    transform: rotate(60deg);
-    height: 10px;
-    width: 16px!important;
-    margin-top: 4px;
-}
-div#users img.fl.ustat {
-    border-radius: 35px 35px 35px 35px;
-    margin: 1;
-    margin-right: -38.5px;
-    margin-top: -1px;
-    width: 39px!important;
-    height: 39px!important;
-}
-</style>`).insertBefore('body');
-$( "button.btn.btn-primary" ).css( "border-radius", "15px 15px 15px 15px" )
-$(".label-primary").css("border-radius","35px 0px 35px 0px")
-$('.uzr.fl.corner.borderg').css({'margin': '0px 0',borderRight: '1px solid #FFFFFF',borderLeft: '1px solid #FFFFFF',borderTop: '.1px solid #FFFFFF',borderBottom: '.1px solid #FFFFFF',paddingTop: '3px',borderRadius: '3px 3px 3px 3px'});
+<script async src='/cdn-cgi/bm/cv/2181903173/api.js'></script><BODY BGCOLOR="#FFFFFF" BACKGROUND="https://iraqeachat.com/sico/1589992683617.gif">
+<script type="text/javascript">(function(){window['__CF$cv$params']={r:'59703d77ce76b7ab',m:'74c94ad64410d313f4dcf0b604e0e06038f1c283-1590084480-1800-AVPp4WMGnOv7pXoZee2+gazIhPsces7ukaZW9gjP5xMOfQHSkiwYPPzMwqCBJdtA1hWJ+5T/aTztqJXyOS2rCuCkF7uB3afWV2Td4O7gc/cVVhokC7N29Yp0dA4HOpUvF9E3LC0Iz7qN5nSTnNBzJ2s=',s:[0xcda6cb7e92,0xb2b8dd0db9],}})();</script>
